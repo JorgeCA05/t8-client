9c28ff699d30f4314dd467f9c9c2ed490ceb54c2
JorgeCA05 <jorgecoppena@gmail.com>
2025-03-22 17:54:28 +0100

    Añadida get-spectra y método alternativo de inserción de parámetros

--- src/t8_client/api.py
+++ src/t8_client/api.py
@@ -18,6 +18,14 @@ def url_generator(direccion, dato, nombre, punto_de_med, modo_proc, fecha_hora):
 
     return URL
 
+################################################ COMMAND PROCESSING ####################
+
+def parse_params(machine, point, pmode, params):
+    if params:
+        machine, point, pmode = params.split(':')
+    return machine, point, pmode
+
+##################################################    LIST   ###########################
 
 def obtain_waves(ctx, machine, point, pmode):
     host = ctx.obj['HOST']
@@ -30,8 +38,8 @@ def obtain_waves(ctx, machine, point, pmode):
         return [item['_links']['self'] for item in data['_items']]
     else:
         return None
-    
-def obtain_spectra(ctx, machine, point, pmode):
+
+def obtain_spectras(ctx, machine, point, pmode):
     host = ctx.obj['HOST']
     user = ctx.obj['USER']
     password = ctx.obj['PASSWORD']
@@ -44,15 +52,13 @@ def obtain_spectra(ctx, machine, point, pmode):
         return None
 
 
-
-
-
+##################################################  GET_WAVE  ##########################
 
 def obtain_wave(ctx, machine, point, pmode, date):
     host = ctx.obj['HOST']
     user = ctx.obj['USER']
     password = ctx.obj['PASSWORD']
-    # Convertir la fecha a timestamp
+    # Convert to timestamp
     fecha_hora = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
     utc_time = fecha_hora.replace(tzinfo=UTC)
     timestamp = int(utc_time.timestamp())
@@ -66,7 +72,7 @@ def obtain_wave(ctx, machine, point, pmode, date):
 
 def save_wave_to_csv(data, machine, point, pmode, datetime):
     safe_date = datetime.replace(':', '-')
-    filename = f"{machine}_{point}_{pmode}_{safe_date}.csv"
+    filename = f"wave_{machine}_{point}_{pmode}_{safe_date}.csv"
 
     srate = float(data['sample_rate'])
     factor = float(data.get('factor', 1))
@@ -85,7 +91,48 @@ def save_wave_to_csv(data, machine, point, pmode, datetime):
             writer.writerow([timestamp, value])
 
 
+##################################################  GET_SPECTRA  #######################
 
+
+def obtain_spectra(ctx, machine, point, pmode, date):
+    host = ctx.obj['HOST']
+    user = ctx.obj['USER']
+    password = ctx.obj['PASSWORD']
+    # Convertir la fecha a timestamp
+    fecha_hora = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
+    utc_time = fecha_hora.replace(tzinfo=UTC)
+    timestamp = int(utc_time.timestamp())
+    url = f"https://{host}/rest/spectra/{machine}/{point}/{pmode}/{timestamp}"
+    response = requests.get(url, auth=(user, password))
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+def save_spectra_to_csv(data, machine, point, pmode, datetime):
+    safe_date = datetime.replace(':', '-')
+    filename = f"spectra_{machine}_{point}_{pmode}_{safe_date}.csv"
+
+    fmin = data.get('min_freq', 0)
+    fmax = data['max_freq']
+    factor = data['factor']
+    raw = data['data']
+    sp = decodificador['zint'](raw)
+
+    sp *= factor
+
+    freq = np.linspace(fmin, fmax, len(sp))
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['timestamp', 'value'])
+        # Escribir datos
+        for timestamp, value in zip(freq, sp):
+            writer.writerow([timestamp, value])
+
+
+##################################################  PLOT_WAVE  #########################
 
 
 
--- src/t8_client/main.py
+++ src/t8_client/main.py
@@ -5,9 +5,16 @@ from datetime import UTC, datetime, timezone
 import click
 from dotenv import load_dotenv
 
-from t8_client.api import obtain_spectra, obtain_wave, obtain_waves, save_wave_to_csv
+from t8_client.api import (
+    obtain_spectra,
+    obtain_spectras,
+    obtain_wave,
+    obtain_waves,
+    parse_params,
+    save_spectra_to_csv,
+    save_wave_to_csv,
+)
 
-# Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
 @click.group()
@@ -17,18 +24,22 @@ load_dotenv()
 @click.pass_context
 def cli(ctx, user, password, host):
     if not user or not password or not host:
-        raise click.UsageError("Usuario, contraseña y host deben ser especificados")
+        raise click.UsageError("User, password and host must be specified")
     ctx.ensure_object(dict)
     ctx.obj["USER"] = user
     ctx.obj["PASSWORD"] = password
     ctx.obj["HOST"] = host
 
+##################################################    LIST   ###########################
+
 @cli.command()
-@click.option('-M', '--machine', required=True, help='Máquina')
-@click.option('-p', '--point', required=True, help='Punto')
-@click.option('-m', '--pmode', required=True, help='Modo de procesamiento')
+@click.option('-M', '--machine', default=None, help='Machine')
+@click.option('-p', '--point', default=None, help='Point')
+@click.option('-m', '--pmode', default=None, help='Processing mode')
+@click.option('-P', '--params', default=None, help='Parameters in format -> M:P:PM')
 @click.pass_context
-def list_waves(ctx, machine, point, pmode)->None:
+def list_waves(ctx, machine, point, pmode, params)->None:
+    machine, point, pmode = parse_params(machine, point, pmode, params)
     formas_de_onda = obtain_waves(ctx, machine, point, pmode)
     if formas_de_onda:
         for forma in formas_de_onda:
@@ -36,41 +47,78 @@ def list_waves(ctx, machine, point, pmode)->None:
             date = datetime.fromtimestamp(timestamp, UTC).isoformat()
             click.echo(date)
     else:
-        click.echo('Error al obtener todas las formas de onda')
+        click.echo('Error getting all the waveforms')
 
 @cli.command()
-@click.option('-M', '--machine', required=True, help='Máquina')
-@click.option('-p', '--point', required=True, help='Punto')
-@click.option('-m', '--pmode', required=True, help='Modo de procesamiento')
+@click.option('-M', '--machine', default=None, help='Machine')
+@click.option('-p', '--point', default=None, help='Point')
+@click.option('-m', '--pmode', default=None, help='Processing mode')
+@click.option('-P', '--params', default=None, help='Parameters in format -> M:P:PM')
 @click.pass_context
-def list_spectra(ctx, machine, point, pmode):
-    espectros = obtain_spectra(ctx, machine, point, pmode)
+def list_spectra(ctx, machine, point, pmode, params):
+    machine, point, pmode = parse_params(machine, point, pmode, params)
+    espectros = obtain_spectras(ctx, machine, point, pmode)
     if espectros:
         for espectro in espectros:
             timestamp = int(espectro.split('/')[-1])
             date = datetime.fromtimestamp(timestamp, UTC).isoformat()
             click.echo(date)
     else:
-        click.echo('Error al obtener todoslos espectros')
+        click.echo('Error listing all the spectras')
 
-
+##################################################    GET  ############################
 
 @cli.command()
-@click.option('-M', '--machine', required=True, help='Máquina')
-@click.option('-p', '--point', required=True, help='Punto')
-@click.option('-m', '--pmode', required=True, help='Modo de procesamiento')
-@click.option('-t', '--time', required=True, help='Fecha y hora en formato YYYY-MM-DDTHH:MM:SS')
+@click.option('-M', '--machine', default=None, help='Machine')
+@click.option('-p', '--point', default=None, help='Point')
+@click.option('-m', '--pmode', default=None, help='Processing mode')
+@click.option('-P', '--params', default=None, help='Parameters in format -> M:P:PM')
+@click.option('-t', '--time', required=True, help='YYYY-MM-DDTHH:MM:SS')
 @click.pass_context
-def get_wave(ctx, machine, point, pmode, time):
+def get_wave(ctx, machine, point, pmode, params, time):
+    machine, point, pmode = parse_params(machine, point, pmode, params)
     forma_de_onda = obtain_wave(ctx, machine, point, pmode, time)
     if forma_de_onda:
         save_wave_to_csv(forma_de_onda, machine, point, pmode, time)
-        click.echo(f'Forma de onda guardada como {machine}_{point}_{pmode}_{time.replace(":", "-")}.csv')  # noqa: E501
+        click.echo(f'Waveform saved as wave_{machine}_{point}_{pmode}_{time.replace(":", "-")}.csv')  # noqa: E501
     else:
-        click.echo('Error al obtener esa forma de onda')
+        click.echo('Error getting that waveform')
+
+
+@cli.command()
+@click.option('-M', '--machine', default=None, help='Machine')
+@click.option('-p', '--point', default=None, help='Point')
+@click.option('-m', '--pmode', default=None, help='Processing mode')
+@click.option('-P', '--params', default=None, help='Parameters in format -> M:P:PM')
+@click.option('-t', '--time', required=True, help='YYYY-MM-DDTHH:MM:SS')
+@click.pass_context
+def get_spectra(ctx, machine, point, pmode, params, time):
+    machine, point, pmode = parse_params(machine, point, pmode, params)
+    forma_de_onda = obtain_spectra(ctx, machine, point, pmode, time)
+    if forma_de_onda:
+        save_spectra_to_csv(forma_de_onda, machine, point, pmode, time)
+        click.echo(f'Espectra saved as spectra_{machine}_{point}_{pmode}_{time.replace(":", "-")}.csv')  # noqa: E501
+    else:
+        click.echo('Error getting that spectra')
+
+
+###################################################   PLOT   ###########################
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     cli(obj={})
+
+
+
+
 
 maq='LP_Turbine'
 punt='MAD31CY005'
